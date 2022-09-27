7c1912a370aaead2388b103a6c85b066561fc754
dinozoff1981 <51277761+dinozoff1981@users.noreply.github.com>
2022-09-27 21:53:20 +0200

    dbconn

--- connect.php
+++ connect.php
@@ -1,10 +1,14 @@
 <?php
 
- $servername="localhost";
-$username="root";
-$password="";
-$database="shalomdb";
+ //$servername="localhost";
+//$username="root";
+//$password="";
+//$database="shalomdb";
 
+$severname='remotemysql.com';
+$database='eaz1Ivi4PE';
+$username='eaz1Ivi4PE';
+$password='pgTGK9YvMB';
 
 $con=new mysqli($servername,$username,$password,$database);
 if(!$con){
